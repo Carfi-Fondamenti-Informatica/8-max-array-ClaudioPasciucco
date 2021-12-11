0dc3a42b65e629393fbb40e4c22dcf70676d5521
ClaudioPasciucco <91784159+ClaudioPasciucco@users.noreply.github.com>
2021-12-11 15:49:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int dimensione;
+  cin >> dimensione;
+  float numeri[dimensione];
+  for (int i=0; i<dimensione; i++){
+    cin >> numeri[i];
+  }
+  nMassimo(numeri , dimensione);
+  
+  cout << nMassimo(numeri, dimensione) << endl;
+  
   return 0;
 }
