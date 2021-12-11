b80985aebc310a944798c70fea27d3cee36e42f9
ClaudioPasciucco <91784159+ClaudioPasciucco@users.noreply.github.com>
2021-12-11 15:44:21 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
-
+#include "lib.h"
+float nMassimo (const float numeri[], int dimensione){
+  numeri[dimendione];
+  float max = numeri[0];
+  for (int i=0; i<dimensione; i++){
+    if (max < numeri[i]){
+      max = numeri[i];
+    }
+  }
+  return max;
+}
